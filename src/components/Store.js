89aea4eb188c49a00af83eccea08d36ed89d301c
Acobeats <fgarcialollini@gmail.com>
2022-10-21 14:36:56 -0300

    New store add

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,34 +1,22 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import { Navbar } from "./Home";
-import Players from '../Styles/Store.css';
+import Players from "../Styles/Store.css";
 
 function Store() {
-    return(
-        <div className="home-div">
-            <ul>
-                <iframe src="//www.beatstars.com/embed/track/?id=10966808"
-                width="55%"
-                height="130">
-                </iframe>
-                <iframe src="//www.beatstars.com/embed/track/?id=6376388"
-                width="55%"
-                height="130">
-                </iframe>
-                <iframe src="//www.beatstars.com/embed/track/?id=6339827"
-                width="55%"
-                height="130">
-                </iframe>
-                <iframe src="//www.beatstars.com/embed/track/?id=6290170"
-                width="55%"
-                height="130">
-                </iframe>
-            </ul>
-            <div>
-                    <Link to='/home'>Go Back</Link>
-                </div>
-        </div>
-    );
+  return (
+    <div className="home-div">
+      <iframe
+        src="https://player.beatstars.com/?storeId=133113"
+        width="100%"
+        height="800"
+      ></iframe>
+
+      <div>
+        <Link to="/home">Go Back</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Store;
